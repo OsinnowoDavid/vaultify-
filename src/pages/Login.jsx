a63429ac79b7b4a90973290a3f710ea32979594a
OsinnowoDavid <davidadewale151@gmail.com>
2025-04-23 16:15:02 +0100

    eyes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,8 +8,11 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { signInFail,signInSuccess,loginStart } from "../redux/User/userSlice.js";
 import vau from "../assets/images/vau.png"
+import { Eye, EyeOff } from "lucide-react";
 // import vaultify from "../assets/images/vaultify.png"
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
   const dispatch = useDispatch()
 const {loading , error} = useSelector((state) => state.user)
 
@@ -90,7 +93,7 @@ toast.success("Welcome back")
     />
   </div>
 
-  <div className="mb-6">
+  {/* <div className="mb-6">
     <label htmlFor="adminPassword" className="block text-gray-700 font-semibold mb-2">
       Password
     </label>
@@ -103,7 +106,29 @@ toast.success("Welcome back")
       className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       required
     />
-  </div>
+  </div> */}
+  
+<div className="mb-6 relative">
+  <label htmlFor="adminPassword" className="block text-gray-700 font-semibold mb-2">
+    Password
+  </label>
+  <input
+    type={showPassword ? "text" : "password"}
+    id="adminPassword"
+    placeholder="Enter your password"
+    value={adminPassword}
+    onChange={(e) => setPassword(e.target.value)}
+    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
+    required
+  />
+  <button
+    type="button"
+    onClick={() => setShowPassword((prev) => !prev)}
+    className="absolute right-3 top-9 text-gray-600 focus:outline-none"
+  >
+    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+  </button>
+</div>
 
   <button
     type="submit"
